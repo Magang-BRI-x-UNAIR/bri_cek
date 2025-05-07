7964ea99de2327029b81e921ca3525250410d8ca
Ardisa Vian <ardisavian4b@gmail.com>
2025-05-07 14:25:37 +0700

    Added Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:bri_cek/theme/app_theme.dart';
 import 'package:bri_cek/utils/app_size.dart';
+import 'package:bri_cek/screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,13 +28,11 @@ class BRICek extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: const SplashScreen(),
+      home: const LoginScreen(), // Set LoginScreen as the initial screen
       builder: (context, child) {
         AppSize().init(context);
         return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            textScaler: TextScaler.linear(AppSize.isSmallScreen ? 0.9 : 1.0),
-          ),
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
           child: child!,
         );
       },
